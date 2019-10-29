bdfae9200f955d3acc09379a17b3a1ca29ec2060
Gareth George <garethgeorge97@gmail.com>
2019-10-28 17:59:33 -0700

    initial commit

---
+++ precache-manifest.d7d295555c8fe1058f0aa1dd250e614c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef440a79aa2085764741abfaa63025b9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f8eb713329b9659a014d",
+    "url": "/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "218f726cd151913f1108",
+    "url": "/static/css/main.fc450718.chunk.css"
+  },
+  {
+    "revision": "f8eb713329b9659a014d",
+    "url": "/static/js/2.9e76d9e6.chunk.js"
+  },
+  {
+    "revision": "218f726cd151913f1108",
+    "url": "/static/js/main.fdf43447.chunk.js"
+  },
+  {
+    "revision": "875d570ce1b6fc43afc6",
+    "url": "/static/js/runtime-main.e077b804.js"
+  }
+]);
