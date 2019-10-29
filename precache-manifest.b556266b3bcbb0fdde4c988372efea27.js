bdd91a495985290a3c001a53ee88639fe679655a
Gareth George <garethgeorge97@gmail.com>
2019-10-28 18:19:04 -0700

    updated

--- precache-manifest.b556266b3bcbb0fdde4c988372efea27.js
+++ precache-manifest.b556266b3bcbb0fdde4c988372efea27.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "ef440a79aa2085764741abfaa63025b9",
+    "revision": "99ad7ec46a0b566fa54814f805e799fc",
     "url": "/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/css/2.0624914a.chunk.css"
   },
   {
-    "revision": "218f726cd151913f1108",
+    "revision": "ae86608609efab4d3e1f",
     "url": "/static/css/main.fc450718.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.9e76d9e6.chunk.js"
   },
   {
-    "revision": "218f726cd151913f1108",
-    "url": "/static/js/main.fdf43447.chunk.js"
+    "revision": "ae86608609efab4d3e1f",
+    "url": "/static/js/main.fcf378ed.chunk.js"
   },
   {
     "revision": "875d570ce1b6fc43afc6",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d7d295555c8fe1058f0aa1dd250e614c.js"
+  "/precache-manifest.b556266b3bcbb0fdde4c988372efea27.js"
 );
 
 self.addEventListener('message', (event) => {
